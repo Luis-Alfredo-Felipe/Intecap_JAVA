f9db2f64e343862b3510e688dd0d0344578de452
Luis-Alfredo-Felipe <genttilez@gmail.com>
2022-09-03 01:12:38 -0600

    first message

---
+++ src/curso_intecap/Curso_Intecap.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package curso_intecap;
+
+/**
+ *
+ * @author Selecom
+ */
+public class Curso_Intecap {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HOla Mundo Programacion");
+        // TODO code application logic here
+    }
+    
+}
